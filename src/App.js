ea60355270206466588c66a7b4e34b5c651a936d
Sunil B <suband@microsoft.com>
2017-07-04 21:51:09 -0700

    Init

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Edge platform status</h2>
+                </header>
+                <section className="App-intro">
+                    Features
+                </section>
+                <footer>
+                    
+                </footer>
+            </div>
+        );
+    }
+}
+
+export default App;
