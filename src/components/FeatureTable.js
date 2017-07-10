8347289d712b53ffaaa6b2574a659a3db00d90cb
Sunil B <suband@microsoft.com>
2017-07-09 18:29:23 -0700

    Add filtering

--- src/components/FeatureTable.js
+++ src/components/FeatureTable.js
@@ -3,11 +3,33 @@ import {FeatureCategoryRow} from './FeatureCategoryRow';
 import {FeatureRow} from './FeatureRow';
 
 export class FeatureTable extends React.Component {
+
+    filterOutFeature(filterText, feature) {
+        let ret = false;
+        if (feature.name && filterText) {
+            ret = true;
+            if (feature.name.toLowerCase().indexOf(filterText) > -1) {
+                ret = false;
+            }
+            if (!ret && feature.category && feature.summary.toLowerCase().indexOf(filterText) > -1) {
+                ret = false;
+            }
+            if (!ret && feature.ieStatus && feature.ieStatus.priority && feature.ieStatus.priority.toLowerCase().indexOf(filterText) > -1) {
+                ret = false;
+            }
+            if (!ret && feature.ieStatus && feature.ieStatus.text && feature.ieStatus.text.toLowerCase().indexOf(filterText) > -1) {
+                ret = false;
+            }
+        }
+        return ret;
+    }
+
     render() {
         let rows = [];
         let lastCategory = null;
+        let filterText = this.props.filterText && this.props.filterText.toLowerCase();
         this.props.features.forEach((feature) => {
-            if (feature.name.indexOf(this.props.filterText) === -1) {
+            if (this.filterOutFeature(filterText, feature)) {
                 return;
             }
             if (feature.category !== lastCategory) {
