64bf9693030827fa2f1d7485c1c91c58ee95fd65
Sunil B <suband@microsoft.com>
2017-07-16 11:54:17 -0700

    Add runtime caching in service worker for CDN and API calls

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -313,6 +313,20 @@ module.exports = {
       navigateFallbackWhitelist: [/^(?!\/__).*/],
       // Don't precache sourcemaps (they're large) and build asset manifest:
       staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
+      runtimeCaching: [
+          {
+              urlPattern: /^http(s)?:\/\/fonts\.googleapis\.com\//,
+              handler: 'cacheFirst'
+          },
+          {
+              urlPattern: /^http(s)?:\/\/code\.getmdl\.io\//,
+              handler: 'cacheFirst'
+          },
+          {
+              urlPattern: /^http(s)?:\/\/cdn\.rawgit\.com\//,
+              handler: 'cacheFirst'
+          }
+      ]
     }),
     // Moment.js is an extremely popular library that bundles large locale files
     // by default due to how Webpack interprets its code. This is a practical
